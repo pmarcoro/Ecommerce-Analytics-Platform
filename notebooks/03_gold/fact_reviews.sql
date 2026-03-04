fa8b567e59f432389d4168cebd93b3e4aeb1a82d
Pablo Martínez Coro <pmarcoro@gmail.com>
2026-03-04 23:49:16 +0100

    Update fact_reviews in gold

--- notebooks/03_gold/fact_reviews.sql
+++ notebooks/03_gold/fact_reviews.sql
@@ -1,32 +1,44 @@
 -- Databricks notebook source
-CREATE OR REPLACE TABLE marketplace_olist.gold.fact_reviews
-USING DELTA
-AS
-WITH ranked_reviews AS (
+CREATE OR REPLACE TABLE marketplace_olist.gold.fact_reviews AS
 
-  SELECT
-    order_id,
-    review_id,
-    review_score,
-    review_creation_date,
-    review_answer_timestamp,
-    ingestion_ts,
+WITH base_reviews AS (
 
-    ROW_NUMBER() OVER (
-      PARTITION BY order_id
-      ORDER BY review_answer_timestamp DESC
-    ) AS rn
+    SELECT
+        oi.seller_key,
+        r.order_id,
+        r.review_id,
+        r.review_score,
+        r.review_creation_date,
+        r.review_answer_timestamp,
 
-  FROM marketplace_olist.silver.order_reviews
+        ROW_NUMBER() OVER (
+            PARTITION BY r.order_id
+            ORDER BY r.review_creation_date ASC
+        ) AS review_version_number,
+
+        ROW_NUMBER() OVER (
+            PARTITION BY r.order_id
+            ORDER BY r.review_creation_date DESC
+        ) AS rn_desc
+
+    FROM marketplace_olist.silver.order_reviews r
+    JOIN marketplace_olist.gold.fact_order_items oi
+        ON r.order_id = oi.order_id
+
+    WHERE r.review_score IS NOT NULL
 
 )
 
 SELECT
-  order_id,
-  review_id,
-  review_score,
-  review_creation_date AS review_date,
-  review_answer_timestamp,
-  ingestion_ts
-FROM ranked_reviews
-WHERE rn = 1;
+    seller_key,
+    order_id,
+    review_id,
+    review_creation_date,
+    review_score,
+    review_version_number,
+    CASE WHEN review_version_number = 1 THEN TRUE ELSE FALSE END AS is_first_review,
+    CASE WHEN rn_desc = 1 THEN TRUE ELSE FALSE END AS is_latest_absolute
+
+FROM base_reviews;
+
+FROM marketplace_olist.silver.order_reviews;
